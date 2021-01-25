c279b725abb3c4731c9d18ad29448b0b3ff9c2ea
scallum4 <48036416+scallum4@users.noreply.github.com>
2021-01-25 22:13:43 +0000

    Used by Main

---
+++ src/Client/backendInterface.java
@@ -0,0 +1,17 @@
+public interface backendInterface extends java.rmi.Remote{
+
+    //Takes a users login details and returns if they are correct
+    public boolean login(String userName, String password) throws java.rmi.RemoteException;
+
+    //Returns true if account could be created, false otherwise
+    public boolean newAccount(String userName, String password) throws java.rmi.RemoteException;
+
+    //Returns true if log out was successful, false otherwise
+    public boolean logOut(String user, String sessionToken) throws java.rmi.RemoteException;
+
+    //Returns a valid session key if the code is correct
+    public String validateCode(String user, int code) throws java.rmi.RemoteException;
+
+    //Returns true if password was chnaged, false otherwise
+    public boolean changePassword(String userName, String password, String oldPassword) throws java.rmi.RemoteException;
+}
