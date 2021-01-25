f97ee5b5d48bd3dce6e702aed4fe48d535af28dc
scallum4 <48036416+scallum4@users.noreply.github.com>
2021-01-25 21:51:09 +0000

    Add files via upload

--- src/server/backendInterface.java
+++ src/server/backendInterface.java
@@ -7,7 +7,7 @@ public interface backendInterface extends java.rmi.Remote{
     public boolean newAccount(String userName, String password) throws java.rmi.RemoteException;
 
     //Returns true if log out was successful, false otherwise
-    public boolean logOut(String user) throws java.rmi.RemoteException;
+    public boolean logOut(String user, String sessionToken) throws java.rmi.RemoteException;
 
     //Returns a valid session key if the code is correct
     public String validateCode(String user, int code) throws java.rmi.RemoteException;
