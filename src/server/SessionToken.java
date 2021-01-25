587f0957d2b5c1b86a5125119f3673631aa269ef
ConstanceA-L <c.ahlefeldt-laurvig@lancaster.ac.uk>
2021-01-25 21:38:35 +0100

    sessionToken comented

--- src/server/SessionToken.java
+++ src/server/SessionToken.java
@@ -1,8 +1,10 @@
 package server;
 
+import java.beans.Encoder;
 import java.security.SecureRandom;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +17,7 @@ public class SessionToken {
     private StringBuffer stringBuffer;
     private String hexString;
 
-    //Find better storage place for tokens
+    //TODO: Find better storage place for tokens
     public HashMap<String, LocalDateTime> sessionTokens = new HashMap<>();
 
 
@@ -23,6 +25,12 @@ public class SessionToken {
     {
     }
     
+    
+    /** 
+     * A token is valid if it exists and is less than 12 hours old. 
+     * @param sessionToken
+     * @return Returns true os a session token is valid. 
+     */
     public boolean isTokenValid(String sessionToken)
     {
         if(sessionTokens.containsKey(sessionToken))
@@ -30,7 +38,7 @@ public class SessionToken {
             LocalDateTime tokenTime = sessionTokens.get(sessionToken);
             LocalDateTime currentTime = LocalDateTime.now();
 
-            //Find the time between when the token was generate an the current time 
+            //Find the time between when the token was generated an the current time 
             Duration duration = Duration.between(tokenTime, currentTime); 
 
             if (duration.toHours() <= 12) //The token will expire after 12 hours 
@@ -48,6 +56,12 @@ public class SessionToken {
         }
     }
 
+    
+    /** 
+     * This method creates a new session token 
+     * @param username The current users' username
+     * @return The newly creatred session token
+     */
     public String createSessionToken(String username)
     {
         //The sessionToken is a randomly generated string and a username
@@ -57,6 +71,11 @@ public class SessionToken {
         return sessionToken;
     }
 
+    
+    /** 
+     * This method is for when a user needs to log out. 
+     * @param token The current session token 
+     */
     public void removeSessionToken(String token)
     {
         if (this.isTokenValid(token))
@@ -65,15 +84,25 @@ public class SessionToken {
         }
     }
 
-    private String generateToken()
+    
+    /** 
+     * @return Returns a token for the session token 
+     */
+    private byte[] generateToken()
     {
         random = new SecureRandom();
         stringBytes = new byte[8];
         random.nextBytes(stringBytes);
 
-        return this.bytesToString(stringBytes);
+        return stringBytes;
+        //return this.bytesToString(stringBytes);
     }
 
+    
+    /** 
+     * @param bytes
+     * @return A string
+     */
     private String bytesToString(byte[] bytes)
     {
         stringBuffer = new StringBuffer();
@@ -89,22 +118,33 @@ public class SessionToken {
         return stringBuffer.toString();
     }
 
+    private String btoString(byte[] bytes)
+    {
+        java.util.Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+        return encoder.encodeToString(bytes);
+    }
+
     
     /*public static void main(String[] args) 
     {
         SessionToken s = new SessionToken();
         String token = s.createSessionToken("Stanci");
 
-        System.out.println(token);
+        byte[] test = s.generateToken();
+        System.out.println(test);
+        System.out.println(s.btoString(test));
+        System.out.println(s.bytesToString(test));
+
+        //System.out.println(token);
 
         if (s.isTokenValid(token)) 
         {
-            System.out.println("The session is valid");            
+            //System.out.println("The session is valid");            
         } else {
-            System.out.println("The session is not valid");                  
+            //System.out.println("The session is not valid");                  
         }
         
-        try 
+        /*try 
         {
             TimeUnit.MINUTES.sleep(1);
         }
