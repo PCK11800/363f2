5f5018c581ced8182f6502b274370ab080b043bc
scallum4 <48036416+scallum4@users.noreply.github.com>
2021-01-25 14:31:19 +0000

    Added other methods.

--- src/server/Backend.java
+++ src/server/Backend.java
@@ -5,7 +5,7 @@ public class Backend extends java.rmi.server.UnicastRemoteObject implements back
             super();
         }
 
-    public boolean login(String userName, String password)
+    public boolean login(String userName, String password) throws java.rmi.RemoteException
     {
         boolean correctPass = pM.checkIfPasswordIsCorrect(userName, password);
         if(correctPass)
@@ -16,8 +16,29 @@ public class Backend extends java.rmi.server.UnicastRemoteObject implements back
         return correctPass;
     }
 
-    public boolean newAccount(String userName, String password)
+    public boolean newAccount(String userName, String password) throws java.rmi.RemoteException
     {
         return pM.addNewUser(userName, password);
+    }
+
+    //Returns true if log out was successful, false otherwise
+    public boolean logOut(String user) throws java.rmi.RemoteException
+    {
+        if(logout is successful)
+        {
+            //invalidate session key
+            return true;
+        }
+        return false;
+    }
+
+    //Returns a valid session key if the code is correct, return null otherwise
+    public String multiFactor(String user, String code) throws java.rmi.RemoteException
+    {
+        if(code is correct)
+        {
+            return //New session key
+        }
+        return null;
     }
 }
