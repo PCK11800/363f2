3db9748465ca29dc7e16aed4ee1e257abeacd72c
scallum4 <48036416+scallum4@users.noreply.github.com>
2021-01-25 22:15:12 +0000

    Connects to rmi

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,8 +1,23 @@
 import client.*;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.NotBoundException;
+import java.net.MalformedURLException;
 
 public class Main {
 
     public Main(){
+        try{
+            backendInterface backEnd = (backendInterface)
+                    Naming.lookup("rmi://localhost/1099");
+        } catch (RemoteException re) {
+            System.out.println(re);
+        } catch (NotBoundException nbe) {
+            System.out.println("NotBoundException");
+        } catch(MalformedURLException murle) {
+            System.out.println("MalformedURLException");
+        }
+
         LogIn loginPage = new LogIn();
         String user = loginPage.getUser();
         FileViewer fileViewer = new FileViewer(user);
