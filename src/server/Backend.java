fa433e59fff818236152f19f707ac38ae5dbb76a
scallum4 <48036416+scallum4@users.noreply.github.com>
2021-01-26 12:09:01 +0000

    Bug fix.

--- src/server/Backend.java
+++ src/server/Backend.java
@@ -56,7 +56,7 @@ public class Backend extends java.rmi.server.UnicastRemoteObject implements back
     {
         if(mfaMap.get(user) == inputtedCode)
         {
-            sessionKeys.createSessionToken(user);
+            return sessionKeys.createSessionToken(user);
         }
         return null;
     }
