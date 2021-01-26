053d874715702c7c27f4e9c503466719a7ff0547
MoZGoose <33805175+MoZGoose@users.noreply.github.com>
2021-01-26 16:22:00 +0000

    Added permissions storage to SaltHash.java

--- src/server/SaltHash.java
+++ src/server/SaltHash.java
@@ -1,8 +1,10 @@
 import java.io.Serializable;
+import java.util.LinkedList;
 
 public class SaltHash implements Serializable{
     private byte[] salt;
     private byte[] hash;
+    private LinkedList<Integer> permissions = new LinkedList<>();
 
     public SaltHash(byte[] salt, byte[] hash)
     {
@@ -18,5 +20,30 @@ public class SaltHash implements Serializable{
     public byte[] getHash()
     {
         return this.hash;
+    }
+
+    public boolean addPermission(int perm)
+    {
+        if(!permissions.contains(perm) && perm >= 1 && perm <=8)
+        {
+            System.out.println("Permission " + perm + " was addded");
+            return permissions.add(perm);
+        }
+        return false;
+    }
+
+    public boolean removePermission(int perm)
+    {
+        return permissions.remove(new Integer(perm));
+    }
+
+    public boolean isPermitted(int perm)
+    {
+        return permissions.contains(perm);
+    }
+
+    public void clearPermissions()
+    {
+        permissions.clear();
     }
 }
