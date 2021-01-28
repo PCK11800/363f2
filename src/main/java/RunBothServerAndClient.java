30bcc499e730f52a485af8a84b1833e3468c2a2d
PCK11800 <casemcpak@gmail.com>
2021-01-28 18:36:20 +0000

    Simple method to run both client and server at once

---
+++ src/main/java/RunBothServerAndClient.java
@@ -0,0 +1,11 @@
+import client.Client;
+import server.Server;
+
+public class RunBothServerAndClient {
+
+    public static void main(String[] args)
+    {
+        Server server = new Server();
+        Client client = new Client();
+    }
+}
