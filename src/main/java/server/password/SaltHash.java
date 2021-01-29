68abed553c909e090853dc833bffa93e575758d5
MoZGoose <33805175+MoZGoose@users.noreply.github.com>
2021-01-29 16:26:28 +0000

    ported over SaltHash.java

--- src/main/java/server/password/SaltHash.java
+++ src/main/java/server/password/SaltHash.java
@@ -1,44 +1,90 @@
-package server.password;
-
 import java.io.Serializable;
 import java.util.LinkedList;
 
-public class SaltHash implements Serializable {
-
+public class SaltHash implements Serializable{
     private byte[] salt;
     private byte[] hash;
     private LinkedList<Integer> permissions = new LinkedList<>();
+    private int role;
 
-    public SaltHash(byte[] salt, byte[] hash) {
+    public SaltHash(byte[] salt, byte[] hash, int role)
+    {
         this.salt = salt;
         this.hash = hash;
+        this.role = role;
+        assignPermissions(role);
     }
 
-    public byte[] getSalt() {
+    public byte[] getSalt()
+    {
         return this.salt;
     }
 
-    public byte[] getHash() {
+    public byte[] getHash()
+    {
         return this.hash;
     }
 
-    public boolean addPermission(int perm) {
-        if (!permissions.contains(perm) && perm >= 1 && perm <= 8) {
+    public boolean addPermission(int perm)
+    {
+        if(!permissions.contains(perm) && perm >= 1 && perm <=8)
+        {
             System.out.println("Permission " + perm + " was addded");
             return permissions.add(perm);
         }
         return false;
     }
 
-    public boolean removePermission(int perm) {
+    public boolean removePermission(int perm)
+    {
         return permissions.remove(new Integer(perm));
     }
 
-    public boolean isPermitted(int perm) {
+    public boolean isPermitted(int perm)
+    {
         return permissions.contains(perm);
     }
 
-    public void clearPermissions() {
+    public void clearPermissions()
+    {
         permissions.clear();
+    }
+
+    public int getRole()
+    {
+        return this.role;
+    }
+
+    private void assignPermissions(int role)
+    {
+        switch (role) {
+            case 0:
+                permissions.add(1);
+                break;
+        
+            case 1:
+                permissions.add(1);
+                permissions.add(2);
+                permissions.add(3);
+                permissions.add(4);
+                permissions.add(5);
+                permissions.add(6);
+                break;
+            
+            case 2:
+                permissions.add(1);
+                permissions.add(2);
+            
+            case 3:
+                permissions.add(1);
+                permissions.add(2);
+                permissions.add(3);
+                permissions.add(4);
+                permissions.add(5);
+                permissions.add(6);
+                permissions.add(7);
+                permissions.add(8);
+
+        }
     }
 }
