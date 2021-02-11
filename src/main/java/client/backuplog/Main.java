49eacf40be4370dce0dae7cfae8bf5765a81e9ac
meLancaster <47994711+meLancaster@users.noreply.github.com>
2021-02-11 02:36:53 +0200

    Update Main.java

--- src/main/java/client/backuplog/Main.java
+++ src/main/java/client/backuplog/Main.java
@@ -5,7 +5,7 @@ import java.rmi.registry.LocateRegistry;
 // Steps:
 // 1. Run Main.java
 // 2. Run Server.java
-// 3. Run BackupClass.java main method, and then needed method (createBackup() or getBackup())
+// 3. Run BackupClass.java main method, and then needed method (createBackup() or getBackup()) in that same class
 
 // The Jgroups .jar file needs to be used as well.
 
