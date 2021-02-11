b6bdd8321eecb6d023c3fb0e072f561128db82c2
meLancaster <47994711+meLancaster@users.noreply.github.com>
2021-02-11 15:46:57 +0200

    Delete src/main/java/client/backuplog directory

--- src/main/java/client/backuplog/Interface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.RemoteException;
-import java.io.File;
-import java.rmi.Remote;
-
-public interface Interface extends Remote {
-
-    public File getBackupFile() throws RemoteException, Exception;
-    public void createBackup(File file) throws RemoteException, Exception;
-}
