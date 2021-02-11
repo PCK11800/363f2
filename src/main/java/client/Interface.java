b42d9800cb3b1614120693b22e1f2870b75319f8
meLancaster <47994711+meLancaster@users.noreply.github.com>
2021-02-11 15:45:31 +0200

    new backup log file

---
+++ src/main/java/client/Interface.java
@@ -0,0 +1,11 @@
+package client.backuplog;
+
+import java.rmi.RemoteException;
+import java.io.File;
+import java.rmi.Remote;
+
+public interface Interface extends Remote {
+
+    public File getBackupFile() throws RemoteException, Exception;
+    public void createBackup(File file) throws RemoteException, Exception;
+}
---
+++ src/main/java/client/Main.java
@@ -0,0 +1,23 @@
+package client.backuplog;
+
+import java.rmi.registry.LocateRegistry;
+
+// Steps:
+// 1. Run Main.java
+// 2. Run Server.java
+// 3. Run BackupClass.java main method, and then needed method (createBackup() or getBackup()) in that same class
+
+// The Jgroups .jar file needs to be used as well.
+
+public class Main
+{
+    public static void main(String[] args) {
+
+        try {
+        LocateRegistry.createRegistry(1099).rebind("project", new ServerCaller());
+        }
+        catch (Exception exception) {
+        System.out.println("registry problem... full trace: " + exception);
+        }
+    }
+}
